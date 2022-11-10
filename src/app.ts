1ea716bf4a1c3f4bff8a572e6df1d0a48c9fea09
duygueroglu <duygu-1999-@hotmail.com>
2022-11-10 23:50:37 +0300

    app.ts and default.ts updated

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
 console.log('message')
+
+import express from 'express'
+import config from "config"
+
+const port = config.get("port") as number;
+const host = config.get("host") as string;
+
+const app = express();
+app.use(deserializeUser);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, host, () => {
+  log.info(`Server listing at http://${host}:${port}`);
+  connect();
+  routes(app);
+});
